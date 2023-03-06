cb728a3f535e51454bc1c4deaef7c9ee9121c55f
rsvaco <48381233+rsvaco@users.noreply.github.com>
2023-03-06 19:46:27 +0100

    añadidas relaciones entre productos y categorias

--- trovador/catalogo/admin.py
+++ trovador/catalogo/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from catalogo.models import Producto, Categoria
 
-class CategoriaAdmin:
+class CategoriaAdmin(admin.ModelAdmin):
     list_display = ("nombre", "descripcion_corta")
-    prepopulated_fields = {"meta_titulo": ("nombre")}
+    prepopulated_fields = {"meta_titulo": ("nombre",)}
+
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ("nombre")
+    prepopulated_fields = {"meta_titulo": ("nombre",)}
 
 # Register your models here.
 admin.site.register(Producto)
+admin.site.register(Categoria, CategoriaAdmin)
--- trovador/catalogo/models.py
+++ trovador/catalogo/models.py
@@ -4,6 +4,30 @@ from django.db import models
 from django.urls import reverse
 
 # Create your models here.
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=50, help_text="Nombre de la categoria")
+    descripcion_corta = models.TextField(max_length=200, blank=True, help_text="Descripcion corta (max 200 caracteres)")
+    #Descripcion larga deberia ser HTML: https://pypi.org/project/django-tinymce/
+    descripcion_larga = models.TextField(max_length=1000, blank=True, help_text="Descripcion larga")
+    meta_titulo = models.SlugField(null=False, unique=True)
+
+    fecha_creacion = models.DateField(auto_now_add=True, help_text="Fecha de creación del producto")
+
+    categoria_padre = models.ForeignKey('self', null=True, blank=True, related_name='categoria', on_delete=models.SET_NULL)
+
+    # Métodos
+    def get_absolute_url(self):
+         """
+         Devuelve la url para acceder a una instancia particular de MyModelName.
+         """
+         return reverse('categoria', args=[str(self.meta_titulo)])
+
+    def __str__(self):
+        """
+        Cadena para representar el objeto MyModelName (en el sitio de Admin, etc.)
+        """
+        return self.nombre
+
 class Producto(models.Model):
     nombre = models.CharField(max_length=50, help_text="Nombre del producto")
     precio = models.FloatField(help_text="Precio del producto")
@@ -14,6 +38,9 @@ class Producto(models.Model):
     imagen2 = models.ImageField(upload_to='uploads/', blank=True)
     imagen3 = models.ImageField(upload_to='uploads/', blank=True)
 
+    meta_titulo = models.SlugField(null=False, unique=True)
+
+    categoria = models.ManyToManyField(Categoria)
 
     class Meta:
         ordering = ["-fecha_creacion"]
@@ -23,7 +50,7 @@ class Producto(models.Model):
          """
          Devuelve la url para acceder a una instancia particular de MyModelName.
          """
-         return reverse('producto', args=[str(self.id)])
+         return reverse('producto', args=[str(self.meta_titulo)])
 
     def __str__(self):
         """
@@ -31,13 +58,3 @@ class Producto(models.Model):
         """
         return self.nombre
 
-class Categoria(models.Model):
-    nombre = models.CharField(max_length=50, help_text="Nombre de la categoria")
-    descripcion_corta = models.TextField(max_length=200, help_text="Descripcion corta (max 200 caracteres)")
-    #Descripcion larga deberia ser HTML: https://pypi.org/project/django-tinymce/
-    descripcion_larga = models.TextField(max_length=1000, help_text="Descripcion larga")
-    meta_titulo = models.SlugField(null=False, unique=True)
-
-    fecha_creacion = models.DateField(auto_now_add=True, help_text="Fecha de creación del producto")
-
-    categoria_padre = models.ForeignKey('self', null=True, related_name='categoria', on_delete=models.SET_NULL)
