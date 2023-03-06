74bb16957fde59c60f5ead746ccf34f466fb5fd7
rsvaco <48381233+rsvaco@users.noreply.github.com>
2023-03-06 20:39:20 +0100

    añadido campo destacado a productos y categorias

--- trovador/catalogo/admin.py
+++ trovador/catalogo/admin.py
@@ -2,11 +2,11 @@ from django.contrib import admin
 from catalogo.models import Producto, Categoria
 
 class CategoriaAdmin(admin.ModelAdmin):
-    list_display = ("nombre", "descripcion_corta")
+    list_display = ("nombre", "descripcion_corta", "destacado")
     prepopulated_fields = {"meta_titulo": ("nombre",)}
 
 class ProductoAdmin(admin.ModelAdmin):
-    list_display = ("nombre",)
+    list_display = ("nombre", "precio", "destacado", "activo")
     prepopulated_fields = {"meta_titulo": ("nombre",)}
 
 # Register your models here.
--- trovador/catalogo/models.py
+++ trovador/catalogo/models.py
@@ -10,6 +10,7 @@ class Categoria(models.Model):
     #Descripcion larga deberia ser HTML: https://pypi.org/project/django-tinymce/
     descripcion_larga = models.TextField(max_length=1000, blank=True, help_text="Descripcion larga")
     meta_titulo = models.SlugField(null=False, unique=True)
+    destacado = models.BooleanField(help_text="Destacado, aparece en la pagina principal")
 
     fecha_creacion = models.DateField(auto_now_add=True, help_text="Fecha de creación del producto")
 
@@ -18,13 +19,13 @@ class Categoria(models.Model):
     # Métodos
     def get_absolute_url(self):
          """
-         Devuelve la url para acceder a una instancia particular de MyModelName.
+         Devuelve la url para acceder a una Categoria
          """
          return reverse('categoria', args=[str(self.meta_titulo)])
 
     def __str__(self):
         """
-        Cadena para representar el objeto MyModelName (en el sitio de Admin, etc.)
+        Cadena para representar el objeto (en el sitio de Admin, etc.)
         """
         return self.nombre
 
@@ -32,6 +33,7 @@ class Producto(models.Model):
     nombre = models.CharField(max_length=50, help_text="Nombre del producto")
     precio = models.FloatField(help_text="Precio del producto")
     activo = models.BooleanField(help_text="Activo, aparece en catalogo")
+    destacado = models.BooleanField(help_text="Destacado, aparece en la pagina principal")
     stock = models.IntegerField(help_text="Unidades en stock")
     fecha_creacion = models.DateField(auto_now_add=True, help_text="Fecha de creación del producto")
     imagen1 = models.ImageField(upload_to='uploads/')
@@ -48,13 +50,13 @@ class Producto(models.Model):
     # Métodos
     def get_absolute_url(self):
          """
-         Devuelve la url para acceder a una instancia particular de MyModelName.
+         Devuelve la url para acceder a una instancia particular de Producto.
          """
          return reverse('producto', args=[str(self.meta_titulo)])
 
     def __str__(self):
         """
-        Cadena para representar el objeto MyModelName (en el sitio de Admin, etc.)
+        Cadena para representar el objeto Producto (en el sitio de Admin, etc.)
         """
         return self.nombre
 
