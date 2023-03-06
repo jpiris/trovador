47fa414b56c61f7dbf812e1d68506ec47cc77074
rsvaco <48381233+rsvaco@users.noreply.github.com>
2023-03-06 23:08:19 +0100

    añadidas vistas detalle primitivas para categorias y productos

--- trovador/catalogo/urls.py
+++ trovador/catalogo/urls.py
@@ -6,6 +6,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('productos/', views.productos, name='productos'),
+    path('producto/<slug:slug>/', views.producto_detail, name='producto'),
+    path('categoria/<slug:slug>/', views.categoria_detail, name='categoria'),
     path('contacto/', views.contacto, name='contacto'),
     path('sobre-nosotros/', views.sobrenosotros, name='sobre-nosotros'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- trovador/catalogo/views.py
+++ trovador/catalogo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.db.models import Count
 from catalogo.models import Producto, Categoria
 
@@ -14,11 +14,20 @@ def productos(request):
         'productos_list': Producto.objects.filter(activo=True).order_by('-id')[:4],
         
         # ordenar categorias a traves de una @property: https://stackoverflow.com/questions/42476581/how-do-i-sort-the-queryset-by-a-propertys-property-django
-        'categorias_destacadas': sorted(Categoria.objects.filter(destacado=True), key = lambda x: x.productos_count) 
+        'categorias_destacadas': sorted(Categoria.objects.filter(destacado=True), key = lambda x: -(x.productos_count)) 
         # intentos fallidos de ordenar: order by solo funciona con valores almacenados en sql
         # .annotate(num_productos='categoria_productos_count'), #.order_by('num_productos')#order_by('-productos_count')  #
     }
     return render(request, 'productos.html', context)
+
+def producto_detail(request, slug):
+    producto = get_object_or_404(Producto, meta_titulo=slug)
+    return render(request, 'detalles/detalles_producto.html', context={'producto': producto})
+
+def categoria_detail(request, slug):
+    categoria = get_object_or_404(Categoria, meta_titulo=slug)
+    categorias_hijas = Categoria.objects.filter(categoria_padre=categoria)
+    return render(request, 'detalles/detalles_categoria.html', context={'categoria': categoria, 'categorias_hijas':categorias_hijas})
 
 def contacto(request):
     return render(request, 'contacto.html')
