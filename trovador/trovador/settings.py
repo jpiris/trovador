686448511db611e7fa64d20f4346a7217b14576c
rsvaco <48381233+rsvaco@users.noreply.github.com>
2023-04-15 17:26:52 +0200

    cambio localizacion archivos estaticos

--- trovador/trovador/settings.py
+++ trovador/trovador/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'catalogo/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
@@ -143,3 +143,6 @@ APPEND_SLASH=True
 
 # Para que la web pueda enviar emails
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# Para django-registration
+ACCOUNT_ACTIVATION_DAYS = 30 # Un mes ventana de activacion
